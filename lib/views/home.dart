6d9a94ec653be3467040251fd0844d666a998b02
Kuldeep singh <singh.kuldeep22798@gmail.com>
2023-09-16 02:50:59 +0530

    change the ui

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,8 @@
 import 'package:calculator_app/controller/homecontroller.dart';
+import 'package:calculator_app/utils/images.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 
 class Home extends StatelessWidget {
@@ -26,31 +28,26 @@ class Home extends StatelessWidget {
                 ),
                 margin: EdgeInsets.zero,
               child: Container(
-                height: MediaQuery.sizeOf(context).height/1.1,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  // color: Colors.green,
-                  borderRadius: BorderRadius.only(topRight: Radius.circular(40),topLeft: Radius.circular(40))
-                ),
+                height: MediaQuery.sizeOf(context).height/1.2,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
 
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-
-                        Obx(()=>Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Align(
-                            alignment: Alignment.bottomRight,
-                              child: Text(_homeController.store.join().toString(),style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),)),
-                        )),
+                          Obx(()=>Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Align(
+                              alignment: Alignment.bottomRight,
+                                child: Text(_homeController.store.join().toString(),style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),)),
+                          )),
 
 
-                        Padding(
-                          padding: EdgeInsets.only(left: 20,right: 20,bottom: 20),
-                          child: Expanded(
+                          Padding(
+                            padding: EdgeInsets.only(left: 20,right: 20,bottom: 20),
                             child: GridView.builder(
                               shrinkWrap: true,
                               physics: NeverScrollableScrollPhysics(),
@@ -105,8 +102,8 @@ class Home extends StatelessWidget {
                               },
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -116,15 +113,20 @@ class Home extends StatelessWidget {
           Align(
             alignment: Alignment.topCenter,
             child: SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Wings",style: TextStyle(color: Colors.green.shade800,fontWeight: FontWeight.bold,fontSize: 30)),
-                    Text("cal",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 30)),
-                  ],
-                ),
+              child: Column(mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Lottie.asset(Images.logo,height: 50),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Wings",style: TextStyle(color: Colors.green.shade800,fontWeight: FontWeight.bold,fontSize: 30)),
+                        Text("cal",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 30)),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           )
